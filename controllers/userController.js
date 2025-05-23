2b0f8c69d66fe78dc1c393667542736f4a11bbf7
Harsh122002.git <chavadaharsh22@gmail.com.git>
2025-05-23 21:47:56 +0530

    delete router update.

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,13 +60,13 @@ export const UpdateUser = async (req, res) => {
 export const DeleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await User.findById(id);
+    const user = await User.findByIdAndDelete(id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    await user.remove();
     return res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
