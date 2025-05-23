73d36178e3e876a26fff5ce0bbb34de7c36bdff9
Harsh122002.git <chavadaharsh22@gmail.com.git>
2025-05-23 18:35:55 +0530

    add three routes.

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,3 +24,52 @@ export const GetAllUsers = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const GetUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json(user);
+  }
+  catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+export const UpdateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (password) user.password = password;
+    await user.save();
+
+    return res.status(200).json({ message: "User updated successfully", user });
+  }
+  catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+export const DeleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    await user.remove();
+    return res.status(200).json({ message: "User deleted successfully" });
+  }
+  catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { CreateUser, GetAllUsers } from "../controllers/userController.js";
+import { CreateUser, DeleteUser, GetAllUsers, GetUserById, UpdateUser } from "../controllers/userController.js";
 
 router.post("/add", CreateUser)
 router.get("/getAll", GetAllUsers)
+router.get("/get/:id", GetUserById)
+router.put("/update/:id", UpdateUser)
+router.delete("/delete/:id", DeleteUser)
 
 export default router;
