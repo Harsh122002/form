48f357ac09ac4680e6f6a49f7da245e636230062
Harsh122002.git <chavadaharsh22@gmail.com.git>
2025-05-23 20:54:02 +0530

    same change.

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ export const CreateUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
     const user = await User.create({ name, email, password });
-    return res.status(201).json({ message: "User created successfully", user });
+    return res.status(201).json({ user });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
   }
@@ -51,7 +51,7 @@ export const UpdateUser = async (req, res) => {
     if (password) user.password = password;
     await user.save();
 
-    return res.status(200).json({ message: "User updated successfully", user });
+    return res.status(200).json({  user });
   }
   catch (error) {
     return res.status(500).json({ message: "Internal server error" });
